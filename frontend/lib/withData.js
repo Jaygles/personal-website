21f1a0c9b01603f83f6cbeadb54259ad1590e84b
Jay Sullivan <JaySull514@gmail.com>
2018-10-07 12:19:21 -0600

    Scaffolding

---
+++ frontend/lib/withData.js
@@ -0,0 +1,23 @@
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
+import { endpoint, prodEndpoint } from '../config';
+
+function createClient({ headers }) {
+  return new ApolloClient({
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+    request: operation => {
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include',
+        },
+        headers,
+      });
+    },
+    // local data
+    clientState: {
+      defaults: {},
+    },
+  });
+}
+
+export default withApollo(createClient);
